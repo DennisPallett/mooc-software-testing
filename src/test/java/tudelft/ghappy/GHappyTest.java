af08abda11a7a147d034c17b0e6692d3dc66c867
Dennis Pallett <dennis.pallett@topicus.nl>
2018-11-12 15:43:09 +0100

    fix ghappy implementation + unit tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,28 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.mirror.Mirror;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "input={0}, expected={1}")
+    @CsvSource(
+            {"xxggxx,true",
+            "xxgxx,false",
+            "xxggyygxx,false",
+            "g,false",
+            "ggg,true"})
+    public void testMirror (String input, String expected) {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy(input);
+
+        if (Boolean.parseBoolean(expected))
+            Assertions.assertTrue(result);
+        else
+            Assertions.assertFalse(result);
+    }
+
 }
