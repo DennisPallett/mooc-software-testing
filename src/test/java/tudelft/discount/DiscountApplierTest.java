acba2761420cc37b1ee58d1415417c42780fb473
Dennis Pallett <dennis.pallett@topicus.nl>
2018-11-28 23:09:36 +0100

    Fix bug in DiscountApplier + unit tests

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,65 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class DiscountApplierTest {
+
+    @Test
+    public void homePriceIsSetCorrectlyTest () {
+        // Arrange
+        Product product = new Product("test", 1.0d, "HOME");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        List<Product> results = Arrays.asList(product);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        // Act
+        DiscountApplier applier = new DiscountApplier(dao);
+        applier.setNewPrices();
+
+        // Assert
+        Assertions.assertEquals(0.9d, product.getPrice());
+    }
+
+    @Test
+    public void businessPriceIsSetCorrectlyTest () {
+        // Arrange
+        Product product = new Product("test", 1.0d, "BUSINESS");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        List<Product> results = Arrays.asList(product);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        // Act
+        DiscountApplier applier = new DiscountApplier(dao);
+        applier.setNewPrices();
+
+        // Assert
+        Assertions.assertEquals(1.1d, product.getPrice());
+    }
+
+    @Test
+    public void homeAndBusinessPricesAreSetCorrectlyTest () {
+        // Arrange
+        Product businessProduct = new Product("test", 1.0d, "BUSINESS");
+        Product homeProduct = new Product("test", 1.0d, "HOME");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        List<Product> results = Arrays.asList(businessProduct, homeProduct);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        // Act
+        DiscountApplier applier = new DiscountApplier(dao);
+        applier.setNewPrices();
+
+        // Assert
+        Assertions.assertEquals(1.1d, businessProduct.getPrice());
+        Assertions.assertEquals(0.9d, homeProduct.getPrice());
+    }
 
 }
